fd2312cc14005ab656faaccf83bbc1c8d275f2f8
monk3yd <jegajardog@gmail.com>
2024-10-02 16:41:32 -0300

    Standard configuration
    test
    
    Keep testing
    
    Add some of my options

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,42 +4,88 @@ local opt = vim.o
 local g = vim.g
 
 -- <leader> key. Defaults to `\`. Some people prefer space.
--- g.mapleader = ' '
--- g.maplocalleader = ' '
+g.mapleader = ' '
+g.maplocalleader = ' '
 
 opt.compatible = false
+
+-- Set to true if you have a Nerd Font installed
+g.have_nerd_font = true
 
 -- Enable true colour support
 if fn.has('termguicolors') then
   opt.termguicolors = true
 end
 
+-- [[ Options settings ]]
+-- See :help option-list
 -- See :h <option> to see what the options do
 
 -- Search down into subfolders
 opt.path = vim.o.path .. '**'
 
+-- Show line numbers
 opt.number = true
+
+-- Switch to relative line numbers, to help with jumping
 opt.relativenumber = true
+
+-- Mouse mode, can be useful for resizing split
+opt.mouse = 'a'
+
+-- Don't show vim mode if you have status line
+opt.showmode = false
+
+-- Sync clipboard between OS and Neovim
+opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Show which line your cursor is on
 opt.cursorline = true
+
 opt.lazyredraw = true
-opt.showmatch = true -- Highlight matching parentheses, etc
+
+-- Highlight matching parentheses, etc
+opt.showmatch = true
+
 opt.incsearch = true
 opt.hlsearch = true
 
-opt.spell = true
+-- Case insensitive searching UNLESS \C or one or more capital letters in the search term
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default (?)
+opt.signcolumn = 'yes'
+
+-- Decrease update time
+opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+opt.timeoutlen = 300
+
+-- Check spelling
+opt.spell = false
 opt.spelllang = 'en'
 
 opt.expandtab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 0
+opt.shiftwidth = 4
 opt.foldenable = true
 opt.history = 2000
 opt.nrformats = 'bin,hex' -- 'octal'
+
+-- Save undo history
 opt.undofile = true
+
+-- Configure how new splits should be opened
 opt.splitright = true
 opt.splitbelow = true
+
 opt.cmdheight = 0
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
