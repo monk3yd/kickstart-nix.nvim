3a7d4dd7ae188acfafd7a34106d5588108c3448e
monk3yd <jegajardog@gmail.com>
2024-10-04 00:29:19 -0300

    Update keymaps

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -93,18 +93,22 @@ keymap.set('n', ']L', vim.cmd.llast, { silent = true, desc = 'last [L]oclist ent
 
 -- Resize vertical splits
 local toIntegral = math.ceil
+
 keymap.set('n', '<leader>w+', function()
   local curWinWidth = api.nvim_win_get_width(0)
   api.nvim_win_set_width(0, toIntegral(curWinWidth * 3 / 2))
 end, { silent = true, desc = 'inc window [w]idth' })
+
 keymap.set('n', '<leader>w-', function()
   local curWinWidth = api.nvim_win_get_width(0)
   api.nvim_win_set_width(0, toIntegral(curWinWidth * 2 / 3))
 end, { silent = true, desc = 'dec window [w]idth' })
+
 keymap.set('n', '<leader>h+', function()
   local curWinHeight = api.nvim_win_get_height(0)
   api.nvim_win_set_height(0, toIntegral(curWinHeight * 3 / 2))
 end, { silent = true, desc = 'inc window [h]eight' })
+
 keymap.set('n', '<leader>h-', function()
   local curWinHeight = api.nvim_win_get_height(0)
   api.nvim_win_set_height(0, toIntegral(curWinHeight * 2 / 3))
@@ -115,8 +119,16 @@ keymap.set('n', '<leader>fq', function()
   vim.cmd('fclose!')
 end, { silent = true, desc = '[f]loating windows: [q]uit/close all' })
 
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+
 -- Remap Esc to switch to normal mode and Ctrl-Esc to pass Esc to terminal
-keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'switch to normal mode' })
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 keymap.set('t', '<C-Esc>', '<Esc>', { desc = 'send Esc to terminal' })
 
 -- Shortcut for expanding to current buffer's directory in command mode
@@ -128,46 +140,57 @@ keymap.set('c', '%%', function()
   end
 end, { expr = true, desc = "expand to current buffer's directory" })
 
+-- Tabs
 keymap.set('n', '<space>tn', vim.cmd.tabnew, { desc = '[t]ab: [n]ew' })
 keymap.set('n', '<space>tq', vim.cmd.tabclose, { desc = '[t]ab: [q]uit/close' })
 
+-- Diagnostics
 local severity = diagnostic.severity
 
-keymap.set('n', '<space>e', function()
-  local _, winid = diagnostic.open_float(nil, { scope = 'line' })
+keymap.set('n', '<leader>e', function()
+  local _, winid = diagnostic.open_float(nil, { desc = 'Show diagnostic [E]rror messages', scope = 'line' })
   if not winid then
     vim.notify('no diagnostics found', vim.log.levels.INFO)
     return
   end
   vim.api.nvim_win_set_config(winid or 0, { focusable = true })
 end, { noremap = true, silent = true, desc = 'diagnostics floating window' })
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
 keymap.set('n', '[d', diagnostic.goto_prev, { noremap = true, silent = true, desc = 'previous [d]iagnostic' })
 keymap.set('n', ']d', diagnostic.goto_next, { noremap = true, silent = true, desc = 'next [d]iagnostic' })
+
 keymap.set('n', '[e', function()
   diagnostic.goto_prev {
     severity = severity.ERROR,
   }
 end, { noremap = true, silent = true, desc = 'previous [e]rror diagnostic' })
+
 keymap.set('n', ']e', function()
   diagnostic.goto_next {
     severity = severity.ERROR,
   }
 end, { noremap = true, silent = true, desc = 'next [e]rror diagnostic' })
+
 keymap.set('n', '[w', function()
   diagnostic.goto_prev {
     severity = severity.WARN,
   }
 end, { noremap = true, silent = true, desc = 'previous [w]arning diagnostic' })
+
 keymap.set('n', ']w', function()
   diagnostic.goto_next {
     severity = severity.WARN,
   }
 end, { noremap = true, silent = true, desc = 'next [w]arning diagnostic' })
+
 keymap.set('n', '[h', function()
   diagnostic.goto_prev {
     severity = severity.HINT,
   }
 end, { noremap = true, silent = true, desc = 'previous [h]int diagnostic' })
+
 keymap.set('n', ']h', function()
   diagnostic.goto_next {
     severity = severity.HINT,
@@ -186,6 +209,98 @@ keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'move [u]p half-page and center' })
 keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'move DOWN [f]ull-page and center' })
 keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'move UP full-page and center' })
 
+-- Clear search highlight on <Esc> press in normal mode
+keymap.set('n', '<Esc>', '<CMD>nohlsearch<CR>')
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move focus to the upper window' })
+
+-- Resize with arrows
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", { silent = true })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { silent = true })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Navigate buffers
+-- keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", { silent = true })
+-- keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", { silent = true })
+
+-- Reorder buffers
+-- keymap("n", "<S-A-h>", ":BufferLineMovePrev<CR>", { silent = true })
+-- keymap("n", "<S-A-l>", ":BufferLineMoveNext<CR>", { silent = true })
+
+-- Close buffers
+vim.keymap.set("n", "<S-q>", "<cmd>Bdelete!<CR>", { silent = true })
+
+-- Move text up and down (normal mode)
+vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<CR>==gi", { silent = true })
+vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<CR>==gi", { silent = true })
+
+-- Move text up and down (visual mode)
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==", { silent = true })
+
+-- Move text up and down (visual block mode)
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", { silent = true })
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", { silent = true })
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv", { silent = true })
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv", { silent = true })
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", { silent = true })
+vim.keymap.set("v", ">", ">gv", { silent = true })
+
+-- Press jk fast to enter
+-- vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
+-- Better paste
+-- vim.keymap.set("v", "p", '"_dP', { silent = true })
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Better yank/copy
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Split screen
+vim.keymap.set("n", "<leader>v", ":vsplit<CR>", { silent = true }) -- vertically
+vim.keymap.set("n", "<leader>s", ":split<CR>", { silent = true })  -- horizontally
+
+-- Hold cursor in the same place when J is pressed.
+-- J appends the line below to the end of current line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Hold cursor in the middle when half page jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Allow search terms to stay in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+
 --- Disabled keymaps [enable at your own risk]
 
 -- Automatic management of search highlight
@@ -199,5 +314,3 @@ keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'move UP full-page and center' })
 --   end
 -- end, auto_hlsearch_namespace)
 
--- Clear search highlight on <Esc> press in normal mode
-keymap.set('n', '<Esc>', '<CMD>nohlsearch<CR>')
