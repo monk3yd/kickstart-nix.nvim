18466399cf1d1c8b7e056eb023ab5df20cb7913c
monk3yd <jegajardog@gmail.com>
2025-02-15 21:38:24 -0300

    Fix

--- nvim/after/ufo.lua
+++ nvim/after/ufo.lua
@@ -3,43 +3,7 @@ if vim.g.did_load_ufo_plugin then
 end
 vim.g.did_load_ufo_plugin = true
 
--- local ufo = require('ufo')
--- ufo.setup {
---     open_fold_hl_timeout = 150,
---     close_fold_kinds_for_lt = { 'imports', 'comment'},
---     close_fold_kinds_for_ft = {
---         default = {default = {}}
---     },
---     enable_get_fold_virt_text = false,
---     preview = {
---         win_config = {
---             border = { '', '─', '', '', '', '─', '', '' },
---             winhighlight = 'Normal:Folded',
---             winblend = 0
---         },
---         mappings = {
---             scrollU = '<C-u>',
---             scrollD = '<C-d>'
---         }
---     },
---     provider_selector = function(bufnr, filetype, buftype)
---         return {'treesitter', 'indent'}
---     end
--- }
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true
-}
-local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-    require('lspconfig')[ls].setup({
-        capabilities = capabilities
-        -- you can add other fields for setting up lsp server in this table
-    })
-end
-require('ufo').setup({
+require('ufo').setup {
     open_fold_hl_timeout = 150,
     close_fold_kinds_for_lt = { 'imports', 'comment'},
     close_fold_kinds_for_ft = {
@@ -57,7 +21,42 @@ require('ufo').setup({
             scrollD = '<C-d>'
         }
     },
-})
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+}
+
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.foldingRange = {
+--     dynamicRegistration = false,
+--     lineFoldingOnly = true
+-- }
+-- local language_servers = vim.lsp.get_clients() -- or list servers manually like {'gopls', 'clangd'}
+-- for _, ls in ipairs(language_servers) do
+--     require('lspconfig')[ls].setup({
+--         capabilities = capabilities
+--         -- you can add other fields for setting up lsp server in this table
+--     })
+-- end
+-- require('ufo').setup({
+--     open_fold_hl_timeout = 150,
+--     close_fold_kinds_for_lt = { 'imports', 'comment'},
+--     close_fold_kinds_for_ft = {
+--         default = {default = {}}
+--     },
+--     enable_get_fold_virt_text = false,
+--     preview = {
+--         win_config = {
+--             border = { '', '─', '', '', '', '─', '', '' },
+--             winhighlight = 'Normal:Folded',
+--             winblend = 0
+--         },
+--         mappings = {
+--             scrollU = '<C-u>',
+--             scrollD = '<C-d>'
+--         }
+--     },
+-- })
 
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open All Folds' })
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close All Folds' })
